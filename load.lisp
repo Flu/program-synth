0e0ebca0dfae922d64998591ebdf455bec37f5b9
Fluturel Adrian <fluturel.adrian@gmail.com>
2019-03-16 18:44:27 +0200

    Added load file

---
+++ load.lisp
@@ -0,0 +1,6 @@
+(in-package :fluturel.program-synth)
+
+(load "/home/fluturel/Lisp/program-synth/packages.lisp")
+(load "~/Lisp/program-synth/functree.lisp")
+(load "~/Lisp/program-synth/genetic.lisp")
+(load "~/Lisp/program-synth/main.lisp")
