f43b6a63bd7b30ceba9f7675bd963c42207db60a
Fluturel Adrian <fluturel.adrian@gmail.com>
2019-03-22 00:26:26 +0200

    Fix load errors

--- load.lisp
+++ load.lisp
@@ -1,6 +1,4 @@
-(in-package :fluturel.program-synth)
-
-(load "/home/fluturel/Lisp/program-synth/packages.lisp")
+(load "~/Lisp/program-synth/packages.lisp")
+(load "~/Lisp/program-synth/main.lisp")
 (load "~/Lisp/program-synth/functree.lisp")
 (load "~/Lisp/program-synth/genetic.lisp")
-(load "~/Lisp/program-synth/main.lisp")
