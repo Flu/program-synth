6c63b3426db8af634f8c379bfecfb50793f933ec
Fluturel Adrian <fluturel.adrian@gmail.com>
2019-03-22 00:31:20 +0200

    Random generators

--- functree.lisp
+++ functree.lisp
@@ -11,7 +11,26 @@
 (defparameter *numeric-constants*
   (range -10 10 0.1))
 
-(defun generate-random-tree(name))
+(defparameter *args*
+  '(_1 _2 _3 _4))
+
+(defmacro get-random-value(l)
+  `(nth (random (length ,l)) ,l))
+
+(defmacro get-value-alist(key alist)
+  `(cdr (assoc ,key ,alist)))
+
+(defun random-function()
+  (car (get-random-value *function-table*)))
+
+(defun random-terminal()
+  (let ((term (random 2)))
+    (if (eql term 0)
+	(get-random-value *args*)
+	(get-random-value *numeric-constants*))))
+
+(defun generate-random-tree(depth))
+;; Generate random code tree with the given functions
 
 (defun change-name(func-tree new-name)
   (setf (cadr func-tree) new-name))
@@ -34,5 +53,8 @@
     ((null tree) nil)
     ((atom tree) (list tree))
     (t (loop :for a :in tree :appending (flatten a)))))
+
+(defun random-subtree(tree))
+  ;; TODO: Select a random subtree and copy it
 
 
