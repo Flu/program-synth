9bd48a9c615050876e51b612d9f5d2cf502880a9
Fluturel Adrian <fluturel.adrian@gmail.com>
2019-04-01 22:25:19 +0300

    ES basics

--- genetic.lisp
+++ genetic.lisp
@@ -1,1 +1,9 @@
 (in-package :fluturel.program-synth)
+
+(defparameter *mutation-chance* 0.2)
+
+(defun mutate(tree)
+  (replace-random-subtree tree (generate-random-tree (3))))
+
+(defun crossover(parent1 parent2)
+  (replace-random-subtree parent1 (random-subtree parent2)))
