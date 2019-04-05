6a938f884cff7f3101982d1bc50a538d3bf073f4
Fluturel Adrian <fluturel.adrian@gmail.com>
2019-04-06 02:40:59 +0300

    Defined the constraints list

--- main.lisp
+++ main.lisp
@@ -1,10 +1,12 @@
 (in-package :fluturel.program-synth)
 
+(defparameter *constraints*
+  '((1 2 0 0 2) (-5 8 0 0 -40) (4 5 0 0 20) (1 -1 0 0 -1) (7 -23 0 0 -161)))
+
 (defun compile-func(func-tree)
   (eval `(defun func(_1 _2 _3 _4) ,func-tree)))
 
-(defun exec-func(func-tree &rest args)
-  (compile-func func-tree)
+(defun exec-func(args)
   (apply #'func args))
 
 (defun range(start end &optional (inc 1))
